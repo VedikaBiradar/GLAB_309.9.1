331edaecce053e30d8d0118ffb985e3d85c4e0ba
VedikaBiradar <vedikabiradar2001@gmail.com>
2024-07-15 18:31:15 -0400

    this is crud by using rest

--- src/main/java/com/vedikabiradar/spring_data_rest_crud_api/Repository/TutorialRepository.java
+++ src/main/java/com/vedikabiradar/spring_data_rest_crud_api/Repository/TutorialRepository.java
@@ -5,12 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
-@Repository
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+@RepositoryRestResource(path = "learningtutorials")
 public interface TutorialRepository extends JpaRepository<Tutorial, Long>
 {
-    //   findByPublished is a custom method()
+    // findByPublished is a custom method()
     List<Tutorial> findByPublished(boolean published);
 }
+
 
 
 
